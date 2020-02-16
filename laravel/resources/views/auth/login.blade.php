b618450de5875c135b13fb1e195819ee98abc4f1
Yamashita <darum@yamas.xyz>
2020-02-16 15:55:07 +0900

    色味の変更

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header text-white bg-dark ">{{ __('Login') }}</div>
+                <div class="card-header text-white bg-success ">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     @isset($message)
